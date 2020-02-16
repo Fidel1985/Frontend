a29714786d413162764c18fffe51e4e532918878
amykytchyn <mykytchyn1985@gmail.com>
2020-02-16 19:25:36 +0200

    added bootstrap styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const inject = require('gulp-inject');
 const browserSync = require('browser-sync').create();
 
 let paths = {
@@ -26,12 +27,19 @@ gulp.task('js', function () {
 
 gulp.task('copy', gulp.series('html', 'css', 'js'));
 
+gulp.task('inject', gulp.series('copy', function () {
+    let target = gulp.src(paths.distIndex);
+    let sources = gulp.src(paths.distCSS, {read: false});
+    return target.pipe(inject(sources, { relative:true }))
+        .pipe(gulp.dest(paths.dist))
+}));
+
 gulp.task('watch', function() {
     browserSync.init({
         server: paths.dist
     });
-    gulp.watch(paths.src, gulp.series('copy'));
+    gulp.watch(paths.src, gulp.series('inject'));
     gulp.watch(paths.dist).on('change', browserSync.reload);
 });
 
-gulp.task('start', gulp.series('copy', 'watch'));
+gulp.task('start', gulp.series('inject', 'watch'));
